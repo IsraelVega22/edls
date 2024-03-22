09ea84f7fe32ecdb946db516bbb8aa8bf7486396
@icenteno <icenteno@ni.thedataage.com>
2024-03-22 15:34:56 -0600

    set struct file

--- main.go
+++ main.go
@@ -3,7 +3,47 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+const Windows = "windows"
+
+type file struct {
+	name             string
+	fileType         int
+	isDir            bool
+	isHidden         bool
+	userName         string
+	groupName        string
+	size             int64
+	modificationTime time.Time
+	mode             string
+}
+
+const (
+	fileRegular int = iota + 1
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileLink
+)
+
+//file extenstion
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	gz  = ".gz"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpp = ".jpg"
+	gif = ".gif"
 )
 
 func main() {
@@ -21,17 +61,100 @@ func main() {
 	flag.Parse()
 
 	path := flag.Arg(0)
+	fmt.Println(path)
 	if path == "" {
 		path = "."
 	}
-
-	fmt.Println(path)
+	fmt.Println("PAth", path)
 	dirs, err := os.ReadDir(path)
 	if err != nil {
 		panic(err)
 	}
+
+	fs := []file{}
+
 	for _, dir := range dirs {
-		fmt.Println(dir.Info())
+		f, err := getFile(dir, false)
+		if err != nil {
+			fmt.Println("Error final", err)
+		}
+		//
+		////add file sliceFile
+		fs = append(fs, f)
 	}
 
+	printList(fs)
+
+	//fmt.Println("Error final", fs)
+
+}
+
+func printList(fs []file) {
+	for _, fl := range fs {
+		fmt.Printf("%s %s %s %10d %s\n", fl.mode, fl.userName, fl.groupName, fl.size, fl.modificationTime.Format("2006-01-02 15:04:05"))
+	}
+}
+
+func getFile(dir fs.DirEntry, isHidden bool) (file, error) {
+	info, err := dir.Info()
+	if err != nil {
+		return file{}, err
+	}
+
+	f := file{
+		name:             dir.Name(),
+		isDir:            dir.IsDir(),
+		isHidden:         isHidden,
+		size:             info.Size(),
+		mode:             info.Mode().String(),
+		modificationTime: info.ModTime(),
+		userName:         "Icenteno",
+		groupName:        "TDA",
+	}
+	//mt.Println(f)
+	setFile(&f)
+
+	return f, nil
+}
+
+func setFile(f *file) {
+	switch {
+	case isLink(*f):
+		f.fileType = fileLink
+	case isExec(*f):
+		f.fileType = fileDirectory
+	case isCompress(*f):
+		f.fileType = fileCompress
+	case isImage(*f):
+		f.fileType = fileImage
+	default:
+		f.fileType = fileRegular
+	}
+
+}
+
+func isLink(f file) bool {
+	return strings.HasPrefix(strings.ToUpper(f.name), "L")
+}
+
+func isExec(f file) bool {
+	if runtime.GOOS == Windows {
+		return strings.HasSuffix(f.name, exe)
+	}
+
+	return strings.Contains(f.mode, "x")
+}
+
+func isCompress(f file) bool {
+	return strings.HasSuffix(f.name, zip) ||
+		strings.HasSuffix(f.name, gz) ||
+		strings.HasSuffix(f.name, tar) ||
+		strings.HasSuffix(f.name, rar) ||
+		strings.HasSuffix(f.name, deb)
+}
+
+func isImage(f file) bool {
+	return strings.HasSuffix(f.name, png) ||
+		strings.HasSuffix(f.name, jpp) ||
+		strings.HasSuffix(f.name, gif)
 }
